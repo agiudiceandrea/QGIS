9a0ecd5c474c20530778052806e067ef638e2774
Andrea Giudiceandrea <andreaerdna@libero.it>
2022-06-23 07:52:11 +0200

    Update src/core/layout/qgslayoutexporter.cpp

--- src/core/layout/qgslayoutexporter.cpp
+++ src/core/layout/qgslayoutexporter.cpp
@@ -595,7 +595,7 @@ QgsLayoutExporter::ExportResult QgsLayoutExporter::exportToPdf( const QString &f
         return FileError;
       }
 
-      p.setRenderHint( QPainter::LosslessImageRendering, mLayout->renderContext().flags() & QgsLayoutRenderContext::FlagLosslessImageRendering );
+      painter.setRenderHint( QPainter::LosslessImageRendering, mLayout->renderContext().flags() & QgsLayoutRenderContext::FlagLosslessImageRendering );
       layerExportResult = printPrivate( printer, p, false, subSettings.dpi, subSettings.rasterizeWholeImage );
       p.end();
       return layerExportResult;
