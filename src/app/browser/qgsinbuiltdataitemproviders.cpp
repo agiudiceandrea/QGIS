e2a51caff4a18f94c13e1704826bd19a667594eb
Andrea Giudiceandrea <andreaerdna@libero.it>
2024-09-09 10:25:18 +0200

    Apply suggestions from code review

--- src/app/browser/qgsinbuiltdataitemproviders.cpp
+++ src/app/browser/qgsinbuiltdataitemproviders.cpp
@@ -432,9 +432,10 @@ void QgsAppDirectoryItemGuiProvider::populateContextMenu( QgsDataItem *item, QMe
   }
   else if ( inFavDirs )
   {
-    
+    QgsDebugMsgLevel( QStringLiteral( "inFavDirs 2" ), 3 );
     if ( QgsFavoriteItem *favoriteItem = qobject_cast< QgsFavoriteItem * >( item ) )
     {
+      QgsDebugMsgLevel( QStringLiteral( "QgsFavoriteItem 2: %1" ).arg( item->type() ), 3 );
       QAction *actionRename = new QAction( tr( "Rename Favorite…" ), menu );
       connect( actionRename, &QAction::triggered, this, [ = ]
       {
